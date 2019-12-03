7d1f1bc0269e6a7cc17790715b71634098ddf72d
J0hnZMT <tuddaojohnzel@gmail.com>
2019-12-03 23:28:12 +0800

    test started

--- Harvest/harvest/test/test_harvest.py
+++ Harvest/harvest/test/test_harvest.py
@@ -1,9 +1,26 @@
 import unittest
+import requests
+from harvest import scraper, downloader, main, logger
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    url = 'http://54.174.36.110/'
+
+    def test_scraper(self):
+        global url
+        resp = requests.get(url)
+        self.assertTrue(scraper.is_good_response(resp))
+        self.assertIsNotNone(scraper.web_scraper(url))
+
+    def test_downloader(self):
+        global url
+        self.assertFalse(downloader.is_url_downloadable(url))
+        self.assertIsInstance(downloader.download_program(url))
+
+    def test_main(self):
+        global url
+        con = scraper.web_scraper(url)
+        self.assertListEqual(main.get_all_link(url, con))
 
 
 if __name__ == '__main__':
